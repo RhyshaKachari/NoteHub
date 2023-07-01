fcec482fc3999f7c36003467a4a2470c60f5dddd
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-01 11:38:56 +0530

    added colors

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color bgColor = Color(0xFF2D033B) ;
+const Color cardColor = Color(0xFF810CA8) ;
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+const Color purple = Color(0XFFC147E9);
+const Color lavender = Color(0XFFE5B8F4);
