a4bfa515636b6124f9f0a45b48a11ea22f77b626
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-01 19:01:44 +0530

    changed the cardColor

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color bgColor = Color(0xFF2D033B) ;
-const Color cardColor = Color(0xFF810CA8) ;
+const Color cardColor = Color(0xFF3B185F) ;
 const Color black = Color(0xFF000000);
 const Color white = Color(0xFFFFFFFF);
 const Color purple = Color(0XFFC147E9);
