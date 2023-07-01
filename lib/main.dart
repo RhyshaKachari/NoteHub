6a34d1e9179da103444260aaa0defcc30fc13729
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-01 19:04:01 +0530

    debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
+
   runApp(const MyApp());
 }
 
@@ -10,7 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
