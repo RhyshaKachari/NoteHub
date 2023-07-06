80a898b75257a9b0e86036f986bc7aa91b018000
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-07 01:42:24 +0530

    added new colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,3 +6,5 @@ const Color black = Color(0xFF000000);
 const Color white = Color(0xFFFFFFFF);
 const Color purple = Color(0XFFC147E9);
 const Color lavender = Color(0XFFE5B8F4);
+const Color skyblue = Color(0XFF78C1F3);
+const Color queenpink = Color(0XFFFF2171);
